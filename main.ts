b902d5fbe26f3b028808c902addf45c80bd85ea1
adrian <16654653+adaviloper@users.noreply.github.com>
2023-04-05 21:22:42 -0500

    initial commit

--- main.ts
+++ main.ts
@@ -1,137 +1,158 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, normalizePath, Notice, Plugin, SuggestModal, TFile } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	suggestionPlaceholder: string;
+	defaultSuggestionId: string;
+}
+
+interface TabCompleteSuggestion {
+	path: string;
+	file?: TFile;
+
+	isDefault: boolean;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	suggestionPlaceholder: 'Create or open note...',
+	defaultSuggestionId: 'default-suggestions',
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'tab-complete-create-or-find-note-command',
+			name: 'Tab Complete: Create of find note',
 			callback: () => {
-				new SampleModal(this.app).open();
+				new TabCompleteSuggestionModal(this.app).open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerDomEvent(document, 'keydown', this.handleTabInput(this.app));
 	}
 
-	onunload() {
-
+	handleTabInput(app: App) {
+		return (event: KeyboardEvent) => {
+			if (event.target.placeholder !== DEFAULT_SETTINGS.suggestionPlaceholder) {
+				return;
+			}
+			if (event.key === 'Tab') {
+				event.preventDefault();
+				const files            = getFiles();
+				const inputValue       = event.target.value;
+				const matchingSuggestions = files.filter(file => file.path.indexOf(inputValue) === 0);
+				if (matchingSuggestions.length === 1) {
+					event.target.value = matchingSuggestions[0].path;
+				} else if (matchingSuggestions.length > 1) {
+					const commonPrefix = this.getCommonPrefix(matchingSuggestions);
+					event.target.value = commonPrefix;
+					matchingSuggestions[0].path = commonPrefix
+					const defaultSuggestion = document.getElementById(DEFAULT_SETTINGS.defaultSuggestionId);
+					if (defaultSuggestion) {
+						defaultSuggestion.innerText = commonPrefix
+					}
+				}
+			}
+		}
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+// Helper function to get common prefix
+	getCommonPrefix(arr: TabCompleteSuggestion[]) {
+		const sortedArr = arr.sort();
+		const first = sortedArr[0], last = sortedArr[arr.length - 1];
+		const L = first.path.length;
+		let i = 0;
+		while (i < L && first.path.charAt(i) === last.path.charAt(i)) i++;
+		return first.path.substring(0, i);
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+	onunload() {}
 }
 
-class SampleModal extends Modal {
+function getFiles(): TabCompleteSuggestion[] {
+	return app.vault.getFiles().map(file => ({
+		path: file.path,
+		file,
+		isDefault: false
+	}));
+}
+
+class TabCompleteSuggestionModal extends SuggestModal<TabCompleteSuggestion> {
 	constructor(app: App) {
 		super(app);
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		this.setPlaceholder(DEFAULT_SETTINGS.suggestionPlaceholder)
+		this.setInstructions([
+			{
+				command: 'Tab',
+				purpose: 'Complete to next match'
+			},
+			{
+				command: 'Enter',
+				purpose: 'Create/Open file'
+			},
+		])
+	}
+
+	getSuggestions (query: string): TabCompleteSuggestion[] {
+		if (query.length === 0) {
+			return [];
+		}
+		const suggestions = getFiles().filter(file => file.path.indexOf(query) === 0);
+		suggestions.unshift({
+			path: query,
+			isDefault: true
+		});
+		return suggestions;
+	}
+
+	renderSuggestion (suggestion: TabCompleteSuggestion, el: HTMLElement) {
+		if (suggestion.isDefault) {
+			el.createEl('div', {
+				text: this.inputEl.value,
+				attr: {
+					id: DEFAULT_SETTINGS.defaultSuggestionId
+				}
+			})
+		} else {
+			el.createEl('div', {
+				text: suggestion.path,
+			})
+		}
+	}
+
+	async onChooseSuggestion (suggestion: TabCompleteSuggestion, event: MouseEvent | KeyboardEvent): any {
+		let note = suggestion.file
+		if (!note) {
+			try {
+				if (suggestion.isDefault) {
+					note = await this.app.vault.create(normalizePath(this.inputEl.value + '.md'), '');
+				}
+			} catch (error) {
+				new Notice(error.message)
+			}
+		} else
+
+		return await this.app.workspace
+			.getLeaf()
+			.openFile(
+				note,
+				{
+					active: true
+				}
+			)
 	}
 
 	onClose() {
 		const {contentEl} = this;
 		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
